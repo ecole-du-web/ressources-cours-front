fe1aa9d502637fd360889ed8b2a72727fb96460a
ziratsu <enzo.ustariz@hotmail.fr>
2020-07-28 21:54:44 +0200

    init

---
+++ SiteFinPartieCSS/SiteKyotoComplet/app.js
@@ -0,0 +1,19 @@
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.liste-nav');
+
+
+
+btnMenu.addEventListener('click', function(){
+
+    menu.classList.toggle('active');
+
+})
+
+
+const allLinks = document.querySelectorAll('.item-nav');
+
+allLinks.forEach(function(item){
+    item.addEventListener('click', function(){
+        menu.classList.toggle('active')
+    })
+})
